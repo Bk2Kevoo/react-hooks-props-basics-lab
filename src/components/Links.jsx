0b99785baaa1007f1bcc67f8c3c38f272ea87d08
kevin fajardo <kevinfajardo1103@gmail.com>
2024-10-09 13:20:12 -0400

    Basics Lab

---
+++ src/components/Links.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Links({ github, linkedin }) {
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={github}>{github}</a> {/* Correct rendering of GitHub link */}
+            <a href={linkedin}>{linkedin}</a> {/* Correct rendering of LinkedIn link */}
+        </div>
+    );
+}
+
+export default Links;
